6ac9d59771baf9604dd964f7646a6068fcc5294c
bmoar <ben@rooted.systems>
2016-02-13 21:57:16 -0500

    add test program to play with the format string

---
+++ otw/vortex/5/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    unsigned char canary[5];
+    unsigned char foo[4];
+
+    memset(foo, "\x00", sizeof (foo));
+    /* 0 *before */ strcpy (canary, "AAAA");
+    printf("%16u%n%32u%n%64u%n%32u%n",
+            1, (int *) &foo[0],
+            1, (int *) &foo[1],
+            1, (int *) &foo[2],
+            1, (int *) &foo[3]);
+    /* 5 *after */ 
+    printf("%02x%02x%02x%02x\n", foo[0], foo[1], foo[2], foo[3]);
+    printf("canary: %02x%02x%02x%02x\n", canary[0], canary[1], canary[2], canary[3]);
+
+}
